1a1910181c656c75d22b2a6dd60522dac2dbd3db
NicolasWltr <nicolas-walter1@gmx.de>
2025-04-03 21:17:43 +0200

    Fixed Header + ADjusted min width for depth 3

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -44,10 +44,9 @@ export class GameComponent implements AfterViewInit{
     this.game.nativeElement.style.maxWidth = size + "px";
     this.game.nativeElement.style.maxHeight = size + "px";
 
-    console.log(size);
     if (size < 400) this.menuHandler.setMaxDepth(1);
     if (size >= 400) this.menuHandler.setMaxDepth(2);
-    if (size >= 1000) this.menuHandler.setMaxDepth(3);
+    if (size >= 800) this.menuHandler.setMaxDepth(3);
   }
 
   @HostListener('window:resize', ['$event'])
